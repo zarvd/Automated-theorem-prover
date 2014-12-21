b386bcf77e7a363949b102f0763425ac911787a5
GallonChoi <404842441@qq.com>
2014-12-21 11:48:11 +0800

    dfs and search_node

--- core.py
+++ core.py
@@ -1,9 +1,11 @@
 import pdb
 
+
 pre_facts = None
 temp_facts = []
 con_fact = None
 IRules = None
+
 
 def dfs(_pre_facts, _con_fact, _IRules):
     # TODO seperate con_fact, Eg: H => G|H or worse
@@ -13,11 +15,14 @@ def dfs(_pre_facts, _con_fact, _IRules):
     pre_facts = _pre_facts
     con_fact = _con_fact
     IRules = _IRules
-    ser_node = con_fact['fact']
-    nodes = [con_fact['fact']]
-    facts = pre_facts
-    result = search_node(ser_node, nodes, facts, [])
-    return result
+    results = None
+
+    while True:
+        results = search_node(con_fact['fact'], [con_fact['fact']], pre_facts, results)
+        if not result:
+            return False
+        elif result[-1]['fact'] is con_fact['fact']:
+            return result
 
 
 def search_node(ser_node, nodes, facts, results):
@@ -25,24 +30,49 @@ def search_node(ser_node, nodes, facts, results):
 
     :param ser_node: target node, a Fact instance
     :param nodes:    store nodes which are related, a list of Fact instances
-    :param facts:    source, a dict of Fact instances
-    :param result:   current result, a dict
+    :param facts:    source, a list
+    :param results:   current results, a list
     """
     if not ser_node:
         return False
 
-    # set temp variable
-    facts_buffer = facts.copy()
-    ser_node_buffer = ser_node
-    nodes_buffer = nodes.copy()
-    result_buffer = result
+    for c_fact in facts:
+        if c_fact['fact'] in nodes:
+            # avoid duplicated related nodes
+            continue
+        fact = c_fact['fact']
+        fact_type = c_fact['type']  # type could be 'input' or 'result'
+        # set temp variable
+        facts_buffer = facts.copy()
+        ser_node_buffer = ser_node
+        nodes_buffer = nodes.copy()
+        result_buffer = result
 
-    for fact in facts:
         if ser_node.value == fact.value:
             # G == G, -G == G
             pass
-            
-            
+        elif ser_node.value in fact.value:
+            # G, G*H
+            nodes_buffer.append(fact)  # store current fact
+            if ser_node.value in fact.left_child.value:
+                ser_node_buffer = fact.right_child
+            else:
+                ser_node_buffer = fact.left_child
+        elif fact.value in ser_node.value:
+            # G*H, G
+            pass
+
+        if ser_node != ser_node_buffer:
+            # nodes update, search next node
+            result = search_node(ser_node_buffer, nodes_buffer, facts, results)
+            if result:
+                return result
+
+    result = test_node(nodes, facts, results)
+    if result:
+        return result
+    return False
+
 
 def test_node(nodes, facts, result):
     # return nodes, facts, result
