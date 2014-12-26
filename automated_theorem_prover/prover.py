5b780c28dcfaf59a9cc39695661ef615dccb9ddc
GallonChoi <404842441@qq.com>
2014-12-26 14:02:04 +0800

    transfer EquiExpression to ImpExpression transfer ImpExpression to OrExpression rule CP

--- automated_theorem_prover/prover.py
+++ automated_theorem_prover/prover.py
@@ -6,28 +6,28 @@ from expression import (Proposition, NotExpression,
 
 class Sequent:
     def __init__(self, premises, conclusion, depth):
-        self.pre = premises
-        self.con = conclusion
+        self.pres = premises
+        self.cons = conclusion
         self.depth = depth
 
     def __eq__(self, other):
-        for pre in self.pre:
-            if pre not in other.pre:
+        for pre in self.pres:
+            if pre not in other.pres:
                 return False
-        for pre in other.pre:
-            if pre not in self.pre:
+        for pre in other.pres:
+            if pre not in self.pres:
                 return False
-        for con in self.con:
-            if con not in other.con:
+        for con in self.cons:
+            if con not in other.cons:
                 return False
-        for pre in other.con:
-            if con not in self.con:
+        for pre in other.cons:
+            if con not in self.cons:
                 return False
         return True
 
     def __str__(self):
-        premises = ', '.join([str(premise) for premise in self.pre])
-        conclusion = ', '.join([str(conclusion) for conclusion in self.con])
+        premises = ', '.join([str(premise) for premise in self.pres])
+        conclusion = ', '.join([str(conclusion) for conclusion in self.cons])
         return premises + ' => ' + conclusion
 
     def __hash__(self):
@@ -50,22 +50,21 @@ def proveSequent(sequent):
         bcolors.print_ok('[%s] %s' % (old_sequent.depth, old_sequent))
 
         # check if this sequent is axiomatically true without unification
-        if len(set(old_sequent.pre.keys()) & set(old_sequent.con.keys())):
+        if len(set(old_sequent.pres.keys()) & set(old_sequent.cons.keys())):
             premises.add(old_sequent)
             continue
 
         while True:
-            # determine which formula to expand
             pre = None
             pre_depth = None
-            for formula, depth in old_sequent.pre.items():
+            for formula, depth in old_sequent.pres.items():
                 if not pre_depth or pre_depth > depth:
                     if not isinstance(formula, Proposition):
                         pre = formula
                         pre_depth = depth
             con = None
             con_depth = None
-            for formula, depth in old_sequent.con.items():
+            for formula, depth in old_sequent.cons.items():
                 if not con_depth or con_depth > depth:
                     if not isinstance(formula, Proposition):
                         con = formula
@@ -84,71 +83,79 @@ def proveSequent(sequent):
                 return False
 
             sequent_a = Sequent(
-                old_sequent.pre.copy(),
-                old_sequent.con.copy(),
+                old_sequent.pres.copy(),
+                old_sequent.cons.copy(),
                 old_sequent.depth + 1
             )
             sequent_b = Sequent(
-                old_sequent.pre.copy(),
-                old_sequent.con.copy(),
+                old_sequent.pres.copy(),
+                old_sequent.cons.copy(),
                 old_sequent.depth + 1
             )
             if apply_pre:
-                del sequent_a.pre[pre]
-                del sequent_b.pre[pre]
+                del sequent_a.pres[pre]
+                del sequent_b.pres[pre]
 
                 if isinstance(pre, NotExpression):
-                    sequent_a.con[pre.formula] = old_sequent.pre[pre] + 1
+                    sequent_a.cons[pre.formula] = old_sequent.pres[pre] + 1
                     conclusion.append(sequent_a)
                     break
                 elif isinstance(pre, AndExpression):
-                    sequent_a.pre[pre.left] = old_sequent.pre[pre] + 1
-                    sequent_a.pre[pre.right] = old_sequent.pre[pre] + 1
+                    sequent_a.pres[pre.left] = old_sequent.pres[pre] + 1
+                    sequent_a.pres[pre.right] = old_sequent.pres[pre] + 1
                     conclusion.append(sequent_a)
                     break
                 elif isinstance(pre, OrExpression):
-                    sequent_a.pre[pre.left] = old_sequent.pre[pre] + 1
-                    sequent_b.pre[pre.right] = old_sequent.pre[pre] + 1
+                    sequent_a.pres[pre.left] = old_sequent.pres[pre] + 1
+                    sequent_b.pres[pre.right] = old_sequent.pres[pre] + 1
                     conclusion.append(sequent_a)
                     conclusion.append(sequent_b)
                     break
                 elif isinstance(pre, ImpExpression):
-                    sequent_a.pre[pre.right] = old_sequent.pre[pre] + 1
+                    # transfer ImpExpression to OrExpression
+                    sequent_a.pres[NotExpression(pre.left)] = old_sequent.pres[pre] + 1
+                    sequent_b.pres[pre.right] = old_sequent.pres[pre] + 1
                     conclusion.append(sequent_a)
+                    conclusion.append(sequent_b)
                     break
                 elif isinstance(pre, EquiExpression):
-                    sequent_a.pre[pre.left] = old_sequent.pre[pre] + 1
-                    sequent_a.pre[pre.right] = old_sequent.pre[pre] + 1
+                    # transfer EquiExpression to ImpExpression
+                    temp = ImpExpression(pre.left, pre.right)
+                    sequent_a.pres[temp] = old_sequent.pres[pre] + 1
+                    temp = ImpExpression(pre.right, pre.left)
+                    sequent_a.pres[temp] = old_sequent.pres[pre] + 1
                     conclusion.append(sequent_a)
                     break
             else:
-                del sequent_a.con[con]
-                del sequent_b.con[con]
+                del sequent_a.cons[con]
+                del sequent_b.cons[con]
                 if isinstance(con, NotExpression):
-                    sequent_a.pre[con.formula] = old_sequent.con[con] + 1
+                    sequent_a.pres[con.formula] = old_sequent.cons[con] + 1
                     conclusion.append(sequent_a)
                     break
                 elif isinstance(con, AndExpression):
-                    sequent_a.con[con.left] = old_sequent.con[con] + 1
-                    sequent_b.con[con.right] = old_sequent.con[con] + 1
+                    sequent_a.cons[con.left] = old_sequent.cons[con] + 1
+                    sequent_b.cons[con.right] = old_sequent.cons[con] + 1
                     conclusion.append(sequent_a)
                     conclusion.append(sequent_b)
                     break
                 elif isinstance(con, OrExpression):
-                    sequent_a.con[con.left] = old_sequent.con[con] + 1
-                    sequent_a.con[con.right] = old_sequent.con[con] + 1
+                    sequent_a.cons[con.left] = old_sequent.cons[con] + 1
+                    sequent_a.cons[con.right] = old_sequent.cons[con] + 1
                     conclusion.append(sequent_a)
                     break
                 elif isinstance(con, ImpExpression):
-                    sequent_a.pre[con.left] = old_sequent.con[con] + 1
-                    sequent_a.con[con.right] = old_sequent.con[con] + 1
+                    # Rule CP
+                    sequent_a.pres[con.left] = old_sequent.cons[con] + 1
+                    sequent_a.cons[con.right] = old_sequent.cons[con] + 1
                     conclusion.append(sequent_a)
                     break
                 elif isinstance(con, EquiExpression):
-                    # sequent_a.pre[con.left] = old_sequent.con[con] + 1
-                    sequent_a.con[con.right] = old_sequent.con[con] + 1
-                    # sequent_a.pre[con.right] = old_sequent.con[con] + 1
-                    sequent_a.con[con.left] = old_sequent.con[con] + 1
+                    # transfer EquiExpression to ImpExpression
+                    temp = ImpExpression(con.left, con.right)
+                    sequent_a.cons[temp] = old_sequent.cons[con] + 1
+                    temp = ImpExpression(con.right, con.left)
+                    sequent_a.cons[temp] = old_sequent.cons[con] + 1
                     conclusion.append(sequent_a)
                     break
     return True
