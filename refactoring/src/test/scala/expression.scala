dfea3d737543f45dcb466f8e8f94eb9eca3c1044
GallonChoi <404842441@qq.com>
2015-04-24 14:38:03 +0000

    add test case

--- refactoring/src/test/scala/expression.scala
+++ refactoring/src/test/scala/expression.scala
@@ -6,12 +6,12 @@ class AtomExprSpec extends FlatSpec with Matchers {
     val exp = new AtomExpression("A")
     assert(exp == exp)
   }
-  "A" should "equals to a copy of A" in {
+  it should "equals to a copy of A" in {
     val exp1 = new AtomExpression("A")
     val exp2 = new AtomExpression("A")
     assert(exp1 == exp2)
   }
-  "A" should "not equals to anything else" in {
+  it should "not equals to anything else" in {
     val exp = new AtomExpression("A")
     val exp2 = new AtomExpression("B")
     val exp3 = new AtomExpression("C")
@@ -21,14 +21,14 @@ class AtomExprSpec extends FlatSpec with Matchers {
     assert(exp != exp3)
     assert(exp != exp4)
   }
-  "A" should "equals to (not (not A))" in {
-    val exp1 = new AtomExpression("A")
-    val exp2 = new NotExpression(exp1)
-    val exp3 = new NotExpression(exp2)
-    assert(exp1 != exp2)
-    assert(exp2 != exp3)
-    assert(exp1 == exp3)
-  }
+  // it should "equals to (not (not A))" in {
+  //   val exp1 = new AtomExpression("A")
+  //   val exp2 = new NotExpression(exp1)
+  //   val exp3 = new NotExpression(exp2)
+  //   assert(exp1 != exp2)
+  //   assert(exp2 != exp3)
+  //   assert(exp1 == exp3)
+  // }
 }
 
 class NotExprSpec extends FlatSpec with Matchers {
@@ -42,21 +42,21 @@ class AndExprSpec extends FlatSpec with Matchers {
     val exp = new AndExpression(atom1, atom2)
     assert(exp == exp)
   }
-  "(A and B)" should "equals to a copy of (A and B)" in {
+  it should "equals to a copy of (A and B)" in {
     val atom1 = new AtomExpression("A")
     val atom2 = new AtomExpression("B")
     val exp1 = new AndExpression(atom1, atom2)
     val exp2 = new AndExpression(atom1, atom2)
     assert(exp1 == exp2)
   }
-  "(A and B)" should "equals to (B and A)" in {
+  it should "equals to (B and A)" in {
     val atom1 = new AtomExpression("A")
     val atom2 = new AtomExpression("B")
     val exp1 = new AndExpression(atom1, atom2)
     val exp2 = new AndExpression(atom2, atom1)
     assert(exp1 == exp2)
   }
-  "(A and B)" should "not equals to anything else" in {
+  it should "not equals to anything else" in {
     val atom1 = new AtomExpression("A")
     val atom2 = new AtomExpression("B")
     val exp1 = new AndExpression(atom1, atom2)
@@ -67,18 +67,47 @@ class AndExprSpec extends FlatSpec with Matchers {
     assert(exp1 != exp3)
     assert(exp1 != exp4)
   }
-  "(A and (B and C))" should "equals to (B and (A and C))" in {
-    val a = new AtomExpression("A")
-    val b = new AtomExpression("B")
-    val c = new AtomExpression("C")
-    val and1 = new AndExpression(a, b)
-    val and2 = new AndExpression(b, c)
-    val and3 = new AndExpression(a, c)
-    val exp1 = new AndExpression(a, and2)
-    val exp2 = new AndExpression(b, and3)
-    val exp3 = new AndExpression(c, and1)
-    assert(exp1 == exp2)
-    assert(exp1 == exp3)
-    assert(exp2 == exp3)
+  // "(A and (B and C))" should "equals to (B and (A and C))" in {
+  //   val a = new AtomExpression("A")
+  //   val b = new AtomExpression("B")
+  //   val c = new AtomExpression("C")
+  //   val and1 = new AndExpression(a, b)
+  //   val and2 = new AndExpression(b, c)
+  //   val and3 = new AndExpression(a, c)
+  //   val exp1 = new AndExpression(a, and2)
+  //   val exp2 = new AndExpression(b, and3)
+  //   val exp3 = new AndExpression(c, and1)
+  //   assert(exp1 == exp2)
+  //   assert(exp1 == exp3)
+  //   assert(exp2 == exp3)
+  // }
+}
+
+
+class OrExprSpec extends FlatSpec with Matchers {
+  "A or B" should "equals to (A or B) itself" in {
+    val atom1 = new AtomExpression("A")
+    val atom2 = new AtomExpression("B")
+    val expr = new OrExpression(atom1, atom2)
+    assert(expr == expr)
+  }
+
+  it should "equals to a copy of (A or B)" in {
+    val atom1 = new AtomExpression("A")
+    val atom2 = new AtomExpression("B")
+    val expr1 = new OrExpression(atom1, atom2)
+    val expr2 = new OrExpression(atom1, atom2)
+    assert(expr1 == expr2)
+  }
+
+  it should "equals to (B or A)" in {
+    val atom1 = new AtomExpression("A")
+    val atom2 = new AtomExpression("B")
+    val expr1 = new OrExpression(atom1, atom2)
+    val expr2 = new OrExpression(atom2, atom1)
+    assert(expr1 == expr2)
+  }
+
+  it should "not equals to anything else" in {
   }
 }
--- refactoring/src/test/scala/prover.scala
+++ refactoring/src/test/scala/prover.scala
@@ -1,3 +1,4 @@
+import scala.language.implicitConversions
 import org.scalatest.{FlatSpec, Matchers}
 import theoremProver._
 
@@ -5,8 +6,7 @@ import theoremProver._
 class ProverSpec extends FlatSpec with Matchers {
   implicit def stringToExpr(s: String): Expression = Parser.process(Parser.fromString(s))
 
-  def prove(expr: Expression): Boolean =
-    Prover.prove(Array(), expr)
+  def prove(expr: Expression): Boolean = Prover.prove(Array(), expr)
 
   "(not G)" should "be unprovable" in {
     val expr = "not G"
@@ -23,28 +23,59 @@ class ProverSpec extends FlatSpec with Matchers {
     assert(prove(expr) == true)
   }
 
-  "((P->Q) and (P->(not Q))) -> (not P)" should "be provable" in {
+  "Negation introduction" should "be provable" in {
     val expr = "((P->Q) and (P->(not Q))) -> (not P)"
     assert(prove(expr) == true)
   }
 
-  "(not P) -> (P -> Q)" should "be provable" in {
+  "Negation elimination" should "be provable" in {
     val expr = "(not P) -> (P -> Q)"
     assert(prove(expr) == true)
   }
 
-  "(not (not P)) -> P" should "be provable" in {
+  "Double negative elimination" should "be provable" in {
     val expr = "(not (not P)) -> P"
     assert(prove(expr) == true)
   }
 
-  "(P and Q) -> (P or Q)" should "be provable" in {
-    val expr = "(P and Q) -> (P or Q)"
+  "Conjunction elimination" should "be provable" in {
+    val expr1 = "(P and Q) -> P"
+    val expr2 = "(P and Q) -> Q"
+    assert(prove(expr1) == true)
+    assert(prove(expr2) == true)
+  }
+
+  "Disjunction elimination" should "be provable" in {
+    val expr = "((P or Q) and (P -> R) and (Q -> R)) -> R"
     assert(prove(expr) == true)
   }
 
-  "((P or Q) and (P -> R) and (Q -> R)) -> R" should "be provable" in {
-    val expr = "((P or Q) and (P -> R) and (Q -> R)) -> R"
+  "Hypothetical syllogism" should "be provable" in {
+    val expr = "((P -> Q) and (Q -> R)) -> (P -> R)"
+    assert(prove(expr) == true)
+  }
+
+  "De Morgan's Theorem" should "be provable" in {
+    val expr1 = "(not (P or Q)) -> ((not P) and (not Q))"
+    val expr2 = "(not (P and Q)) -> ((not P) or (not Q))"
+    assert(prove(expr1) == true)
+    assert(prove(expr2) == true)
+  }
+
+  "Biconditional introduction" should "be provable" in {
+    val expr = "((P -> Q) and (Q -> P)) -> (P <-> Q)"
+    assert(prove(expr) == true)
+  }
+
+  "Biconditional elimination" should "be provable" in {
+    val expr1 = "(P <-> Q) -> (P -> Q)"
+    val expr2 = "(P <-> Q) -> (Q -> P)"
+    assert(prove(expr1) == true)
+    assert(prove(expr2) == true)
+  }
+
+  "Modus ponens" should "be provable" in {
+    val expr = "(P and (P -> Q)) -> Q"
     assert(prove(expr) == true)
   }
 }
